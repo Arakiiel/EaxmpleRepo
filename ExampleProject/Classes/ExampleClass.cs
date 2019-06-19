04138f79260718775cb572ca12dbbb566b8dfc35
Callum Hutchinson <callum-hutchinson@live.co.uk>
2019-06-19 21:24:35 +0100

    final commit

--- ExampleProject/Classes/ExampleClass.cs
+++ ExampleProject/Classes/ExampleClass.cs
@@ -1,11 +1,43 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+#nullable enable
 
 namespace ExampleProject.Classes
 {
-    class FullName
+    struct Name
     {
         public string FirstName { get; set; }
-        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName => FirstName + LastName;
+
+        public readonly string ReturnFullName() => FullName;
+
+        //public readonly string ModifyFullName()
+        //{
+        //    FirstName = FirstName + "asdf";
+        //    return FirstName;
+        //}
+    }
+
+    interface IFullName
+    {
+        string FirstName { get; set; }
+        string MiddleName { get; set; }
+        string LastName { get; set; }
+
+        string ReturnFullName();
+        string ReturnFullName(string randomString)
+        {
+            return FirstName + " " + LastName + " " + randomString;
+        }
+    }
+
+
+    class FullName : IFullName
+    {
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
         public string LastName { get; set; }
 
         public FullName(string firstName, string middleName, string lastName)
@@ -15,9 +47,16 @@ namespace ExampleProject.Classes
             LastName = lastName;
         }
 
+        public void Deconstruct(out string firstName, out string middleName, out string lastName)
+        {
+            firstName = FirstName;
+            middleName = MiddleName;
+            lastName = LastName;
+        }
+
         public string ReturnFullName()
         {
-            return FirstName +" "+ MiddleName+ " " + LastName;
+            return FirstName + " " + MiddleName + " " + LastName;
 
             //return null;
             //return "adsfa";
@@ -28,32 +67,52 @@ namespace ExampleProject.Classes
     class ExampleClass
     {
         public List<FullName> Names { get; set; }
+        public string[] NameArray { get; set; }
+
 
         public ExampleClass()
         {
             Names = new List<FullName>();
+            NameArray = new string[5] { "name 1", "name 2", "name 3", "name 4", "name 5" };
         }
+
+
 
         public void InitialiseNames()
         {
-            Names.Add(new FullName("name1", "name2", "name3"));
-            Names.Add(new FullName("name1", "name2", "name3"));
-            Names.Add(new FullName("name1", "name2", "name3"));
-            Names.Add(new FullName("name1", "name2", "name3"));
-            Names.Add(new FullName("name1", "name2", "name3"));
-            Names.Add(new FullName("name1", "name2", "name3"));
-            Names.Add(new FullName("name1", "name2", "name3"));
-            Names.Add(new FullName("name1", "name2", "name3"));
-            Names.Add(new FullName("name1", "name2", "name3"));
+            Names.Add(new FullName("first name 1", "middle name 1", "last name 1"));
+            Names.Add(new FullName("first name 2", "middle name 2", "last name 2"));
+            Names.Add(new FullName("first name 3", "middle name 3", "last name 3"));
+            Names.Add(new FullName("first name 4", "middle name 4", "last name 4"));
+            Names.Add(new FullName("first name 5", "middle name 5", "last name 5"));
+            Names.Add(new FullName("first name 6", "middle name 6", "last name 6"));
+            Names.Add(new FullName("first name 7", "middle name 7", "last name 7"));
+            Names.Add(new FullName("first name 8", "middle name 8", "last name 8"));
+
         }
 
         public List<FullName> ReturnNames()
         {
             foreach (FullName name in Names)
             {
+                if (name.MiddleName != null)
+                {
+                    name.MiddleName = AlterName(name.MiddleName);
+                }
+
                 name.LastName = AlterName(name.LastName);
             }
             return Names;
+        }
+
+        //async example
+        public async IAsyncEnumerable<string> AsyncExample()
+        {
+            foreach (string s in this.NameArray)
+            {
+                await Task.Delay(1000);
+                yield return s;
+            }
         }
 
         public string AlterName(string name)
@@ -63,7 +122,7 @@ namespace ExampleProject.Classes
 
         public void AlterNames()
         {
-            foreach(FullName name in Names)
+            foreach (FullName name in Names)
             {
                 name.LastName = AlterName(name.LastName);
             }
@@ -74,5 +133,19 @@ namespace ExampleProject.Classes
             string temp = Utils.RandomStringGenerator.RandomString(12);
             var change = "random change";
         }
+
+        //property pattern
+
+        public string PropertyPatternExample(FullName fullName, string additionalString) =>
+            fullName switch
+            {
+                { FirstName: "first name 1" } => "First name found " + additionalString,
+                { FirstName: "first name 2" } => "Second name found " + additionalString,
+                { FirstName: "first name 3" } => "Third name found " + additionalString,
+                _ => "None found"
+            };
+
+
+
     }
 }
--- ExampleProject/Program.cs
+++ ExampleProject/Program.cs
@@ -1,21 +1,78 @@
 ﻿using ExampleProject.Classes;
 using System;
+using System.Collections;
+using System.Linq;
 
 namespace ExampleProject
 {
     class Program
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //static local functions
+            var x = 2;
+
+            //static int TotalValue2(int value1, int value2) => (x + value2);
+            static int TotalValue(int value1, int value2) => (value1 + value2);
+
+            Console.WriteLine("Total value of 12 and 3 is " + TotalValue(12, 3));
+
 
             var examples = new ExampleClass();
             examples.InitialiseNames();
 
-            examples.ReturnNames().ForEach(x => Console.WriteLine(x.ReturnFullName()));
+            //examples.ReturnNames().ForEach(x => Console.WriteLine(x.ReturnFullName()));
 
-            examples.AlterNames();
-            examples.ReturnNames().ForEach(x => Console.WriteLine(x.ReturnFullName()));
+            //examples.AlterNames();
+            //examples.ReturnNames().ForEach(x => Console.WriteLine(x.ReturnFullName()));
+            //var example2 = examples.ReturnNames().FirstOrDefault();
+
+            //default interface example
+            //Console.WriteLine("default interface inplementation");
+            //Console.WriteLine((example2 as IFullName).ReturnFullName("asdf"));
+
+
+
+            //indices and ranges
+            var lastName = examples.NameArray[..^1];
+            var betweenFirstAndLastName = examples.NameArray[0..^1];
+            var everyName = examples.NameArray[..];
+
+            var rangeExamples = 0..3;
+
+            var namesZeroTo4 = examples.NameArray[rangeExamples];
+
+            foreach(string s in lastName)
+            {
+                Console.WriteLine(s);
+            }
+
+            //async example
+            await foreach(string s in examples.AsyncExample())
+            {
+                Console.WriteLine(s);
+            }
+
+            //property pattern
+            Console.WriteLine(examples.PropertyPatternExample(examples.Names.FirstOrDefault(), "random string"));
+
+            if (examples.Names.FirstOrDefault() is FullName { FirstName: "first name 1"})
+            {
+                Console.WriteLine("first name is first name one");
+            }
+
+            //tuple pattern
+            if(("test","test") is ("test", "test"))
+            {
+                Console.WriteLine("wow apparently these values are the same TUPLE");
+            }
+
+            //positional pattern
+            if(examples.Names.FirstOrDefault() is ("first name 1", "middle name 1", "last name 1"))
+            {
+                Console.WriteLine("wow apparently these values are the same POSITIONAL");
+            }
+
         }
     }
 }
